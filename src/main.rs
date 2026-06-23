d40059fd105fa657b2d1ee07ba9c4ca94b58a14b
Andre <andreananda1022@gmail.com>
2026-06-23 16:41:17 +0700

    refactor: use TryFrom implementation for Request

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,94 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufRead, Write};
+use std::net::{
+    TcpListener, TcpStream
+};
+use std::io::{
+    BufReader, BufRead, Write
+};
+use std::collections::HashMap;
+use std::convert::TryFrom;
+
+struct Request {
+    method: String,
+    path: String,
+    version: String,
+    headers: HashMap<String, String>,
+    body: Option<String>,
+}
+
+impl TryFrom<TcpStream> for Request {
+    type Error = std::error::Error;
+
+    fn try_from(stream: TcpStream) -> Result<Self, Self::Error> {
+        let mut reader = BufReader::new(&mut stream);
+
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+
+        let request_line = line.trim_end().to_string();
+        let mut request_line_parts = request_line.split_whitespace();
+
+        let method = request_line_parts.next().unwrap().to_string();
+        let path = request_line_parts.next().unwrap().to_string();
+        let version = request_line_parts.next().unwrap().to_string();
+
+        let mut headers = HashMap::new();
+        loop {
+            let mut header = String::new();
+            reader.read_line(&mut header)?;
+
+            let header = header.trim_end();
+
+            if header.is_empty() {
+                break;
+            }
+
+            if let Some((key, value)) = line.split_once(": ") {
+                headers.insert(key.to_string(), value.to_string());
+            }
+        }
+
+        let content_length: usize = headers
+            .get("Content-Length")
+            .and_then(|val| val.parse().ok())
+            .unwrap_or(0);
+
+        let body = if content_length > 0 {
+            let mut buf = vec![0u8; content_length];
+            reader.read_exact(&mut buf)?;
+
+            Some(String::from_utf8_lossy(&buf).to_string())
+        } else {
+            None
+        };
+
+        Ok(Request {
+            method, path, version, headers, body
+        })
+    }
+}
 
 fn handle_request(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     println!("Accepted request from: {}", stream.peer_addr()?);
 
-    let mut buf_reader = BufReader::new(&mut stream);
+    let request = Request::TryFrom(streami)?;
+    let response = if request.path == String::from("/") {
+        String::from("HTTP/1.1 200 OK\r\n\r\n")
+    } else if request.path == String::from("/echo") || request.path.starts_with("/echo/") {
+        let content = request.path.strip_prefix("/echo/").unwrap_or("");
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            content.len(), content
+        )
+    } else if request.path == String::from("/user-agent") || request.path.starts_with("/user-agent/") {
+        let user_agent = request.headers.get("User-Agent: ")
 
-    let mut request_line = String::new();
-    buf_reader.read_line(&mut request_line)?;
-
-    let mut headers: Vec<String> = Vec::new();
-    for line_result in buf_reader.lines() {
-        let header_line = line_result?;
-        if header_line.is_empty() {
-            break;
-        }
-
-        headers.push(header_line);
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            user_agent.len(), user_agent
+        )
+    } else {
+        String::from("HTTP/1.1 404 Not Found\r\n\r\n")
     }
-
-    let request_parts: Vec<&str> = request_line.split_whitespace().collect();
-    let response = match request_parts[..] {
-        ["GET", path, "HTTP/1.1"] => {
-            if path == "/" {
-                String::from("HTTP/1.1 200 OK\r\n\r\n")
-            } else if path == "/echo" || path.starts_with("/echo/") {
-                let content = path.strip_prefix("/echo/").unwrap_or("");
-                format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    content.len(), content
-                )
-            } else if path == "/user-agent" || path == "/user-agent/" {
-                let content = headers.iter()
-                    .find_map(|h| h.strip_prefix("User-Agent: "))
-                    .unwrap_or("unknown user agent");
-
-                format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    content.len(), content
-                )
-            } else {
-                String::from("HTTP/1.1 404 Not Found\r\n\r\n")
-            }
-        }
-        _ => String::from("HTTP/1.1 400 Bad Request\r\n\r\n")
-    };
 
     stream.write_all(response.as_bytes())?;
     stream.flush()?;
